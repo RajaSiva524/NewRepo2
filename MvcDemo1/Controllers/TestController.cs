0858d5e6866574cc3c5046dfc95e5fed8a49897b
nagaraj.juluri <nagaraj.juluri@gmail.com>
2023-05-28 18:25:49 +0530

    Add project files.

---
+++ MvcDemo1/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo1.Models;
+
+namespace MvcDemo1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<EmployeeModel> emp = new List<EmployeeModel>();
+
+            EmployeeModel emp1 = new EmployeeModel();
+
+            emp1.EmpId = 1;
+            emp1.EmpName = "Suresh";
+            emp1.Salary = 30000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+
+            emp2.EmpId = 2;
+            emp2.EmpName = "Naresh";
+            emp2.Salary = 40000;
+
+            emp.Add(emp1);
+            emp.Add(emp2);
+
+            return View(emp);
+        }
+    }
+}
---
+++ MvcDemo1/Controllers/newController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class newController : Controller
+    {
+        // GET: new
+        public ActionResult TempDataExample()
+        {
+            TempData["student"] = "sandhya";
+            return RedirectToAction("TempDataExample2");
+        }
+        public ActionResult TempDataExample2()
+        {
+            string value = TempData["student"].ToString();
+            TempData.Keep();
+            return Content(value);
+        }
+        public ContentResult content1()
+        {
+            return Content("<h1 style= color:red >welcome</h1>");
+        }
+    }
+}
